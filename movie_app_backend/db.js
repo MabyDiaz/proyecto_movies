c6f93bfa116e9ba9b685eb93372ba134d0d608ff
MabyDiaz <mabeldiaz7680@gmail.com>
2024-07-05 00:17:50 -0300

    update

--- movie_app_backend/db.js
+++ movie_app_backend/db.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { createConnection } from 'mysql2';
+import MySQLStore from 'express-mysql-session';
 
 dotenv.config();
 
@@ -13,6 +14,22 @@ const dbConfig = {
 
 const connection = createConnection(dbConfig);
 
+const sessionStore = new MySQLStore(
+  {
+    expiration: 86400000, // Tiempo de expiración de la sesión (en milisegundos)
+    createDatabaseTable: true, // Crea automáticamente la tabla de sesiones si no existe
+    schema: {
+      tableName: 'sessions',
+      columnNames: {
+        session_id: 'session_id',
+        expires: 'expires',
+        data: 'data',
+      },
+    },
+  },
+  connection
+); // Pasa la conexión de MySQL
+
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to database:', err.stack);
@@ -22,3 +39,13 @@ connection.connect((err) => {
 });
 
 export default connection;
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to database:', err.stack);
+//     return;
+//   }
+//   console.log('Connected to database as id', connection.threadId);
+// });
+
+// export default connection;
